c6bda54563efec0f647c96de0200cf50deb90118
jojo <yza442@sfu.ca>
2020-12-02 22:41:38 -0800

    index animation update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 "use strict";
 
-AOS.init();
+function typeWriter(text, n) {
+    if (n < (text.length)) {
+      $('.test').html(text.substring(0, n+1));
+      n++;
+      setTimeout(function() {
+        typeWriter(text, n)
+      }, 100);
+    }
+  }
+  
+  $(document).ready(function(e) {
+    var text = $('.test').data('text');
+    
+    typeWriter(text, 0);
+  });
   
